49e7b2ed56a69b50af2ceb25eb9289efd60c6b94
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2024-02-23 10:34:00 -0800

    Update copyright to 2024

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -33,7 +33,7 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.2rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>&copy; 2022</p>
+                  <p>&copy; 2024</p>
                </Box>
             </Grid>
          </Grid>
